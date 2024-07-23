52420eeb8ebc2a54b7ab30cf3c852ae0c2814381
Navincitronn <navincitronn@gmail.com>
2024-07-23 17:03:00 -0500

    Updated

--- suggestion.php
+++ suggestion.php
@@ -4,8 +4,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $to = "navincitronn@gmail.com";
     $subject = "New Album Isolation Recommendation";
-    $headers = "From: no-reply@yourdomain.com\r\n";
-    $headers .= "Reply-To: no-reply@yourdomain.com\r\n";
+    $headers = "From: no-reply@navincitron.com\r\n";
+    $headers .= "Reply-To: no-reply@navincitron.com\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     mail($to, $subject, $message, $headers);
